91a9f4d236552d501a30690251f4a3dc07ca1253
Reddy <2480191@cognizant.com>
2026-03-26 20:38:23 +0530

    Added a new code into Merged Entities

--- FolderForEntities/MergedEntities.cs
+++ FolderForEntities/MergedEntities.cs
@@ -1,1 +1,8 @@
-Console.WriteLine("Hello World"); 
+Console.WriteLine("Hello World");  
+class HelloWorldProgram
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
