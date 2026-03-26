274706d819bfd1fe6de0ae0f94da8bf1a8d612e6
Reddy <2480191@cognizant.com>
2026-03-26 15:20:23 +0530

    This is commit after adding Hello folder

--- Product.cs
+++ Product.cs
@@ -1,1 +1,2 @@
-Console.WriteLine("This is a product code from b1"); Console.WriteLine("This is a product code from b2"); 
+Console.WriteLine("This is a product code from b1"); 
+Console.WriteLine("This is a product code from b2"); 
